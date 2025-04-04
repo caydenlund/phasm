0a92f5026a4c33eeb138fd8ffc4e2885c6e3005a
Cayden Lund <caydenlund@gmail.com>
2025-04-03 23:15:30 -0600

    add more static analysis

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
+#![warn(
+    clippy::all,
+    clippy::cargo,
+    clippy::missing_docs_in_private_items,
+    clippy::nursery,
+    clippy::pedantic,
+    missing_docs,
+    rustdoc::all
+)]
+
 pub mod traits;
